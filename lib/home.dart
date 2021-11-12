7b88c07e01630a17648e212fa90095e3c432868a
NourG8 <nourgerfali08@gmail.com>
2021-11-12 19:59:55 +0100

    GridView Update

--- lib/home.dart
+++ lib/home.dart
@@ -82,38 +82,6 @@ class HomePage extends StatelessWidget {
           childAspectRatio: 8.0 / 9.0,
           children: _buildGridCards(10) // Replace
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        padding: const EdgeInsets.all(16.0),
-        childAspectRatio: 8.0 / 9.0,
-        // TODO: Build a grid of cards (102)
-        children: <Widget>[
-          Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                AspectRatio(
-                  aspectRatio: 18.0 / 11.0,
-                  child: Image.asset('assets/diamond.png'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Title'),
-                      const SizedBox(height: 8.0),
-                      Text('Secondary Text'),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          )
-        ],
-        children: <Widget>[Card()],
-      ),
       body: Center(
         child: Text('You did it!'),
       ),
